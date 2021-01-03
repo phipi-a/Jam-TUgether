d5225791ae725181dded565ecac370215f0dc54e
Tomer <t.yavor@gmx.net>
2021-01-03 18:41:39 +0100

    correction

--- pcps-server/js/room.js
+++ pcps-server/js/room.js
@@ -3,14 +3,14 @@ const RoomSchema = require('../model/room.model')
 const { exists } = require('../model/room.model')
 
 // simple push of DB array
-exports.receiveTrack = async function (req, res) {
-  const query = { roomID: req.body.roomID, 'soundtracks.userID': req.body.userID }
+exports.receiveTrack = async function (req, res, roomID) {
+  const query = { roomID: roomID }
   const updateDocument = {
     $push: {
-      'soundtracks.$[].soundseq': { instrument: req.body.instrument, starttime: req.body.starttime, pitch: req.body.pitch }
+      soundtracks: req.body.soundtracks
     }
   }
-  await RoomSchema.updateOne(query, updateDocument)
+  await RoomSchema.updateMany(query, updateDocument)
   res.send('success!')
 }
 
