3711741407adb3b2b0a49d83424471f7e5a75a9d
Yonatan Demeke Demissie <y.demissie@campus.tu-berlin.de>
2021-01-04 22:33:03 +0100

    made moshi adapter lenient

--- pcps-client/app/src/main/java/de/pcps/jamtugether/api/services/soundtrack/SoundtrackService.java
+++ pcps-client/app/src/main/java/de/pcps/jamtugether/api/services/soundtrack/SoundtrackService.java
@@ -5,12 +5,10 @@ import androidx.annotation.NonNull;
 import de.pcps.jamtugether.api.responses.soundtrack.UploadSoundtracksResponse;
 import de.pcps.jamtugether.api.services.soundtrack.bodies.UploadSoundtracksBody;
 import de.pcps.jamtugether.model.Composition;
-import de.pcps.jamtugether.model.soundtrack.SingleSoundtrack;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
