22f6312859b7fd923528d94a11d933f9f4624249
segeth <lns.str@tutanota.com>
2021-01-31 17:31:55 +0100

    removed personal settings

--- pcps-server/db/database.js
+++ pcps-server/db/database.js
@@ -1,11 +1,7 @@
-require('dotenv').config()
-
 module.exports = {
   db: 'mongodb://localhost:27017/jamtugether',
   options: {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user: 'server',
-    pass: process.env.MONGODB_PASSWORD
+    useUnifiedTopology: true
   }
 }
