e717ab2a7a252c4d1e7223135493bb64985518a9
Yoni <y.demissie@campus.tu-berlin.de>
2021-01-31 02:22:15 +0100

    snackbar is shown when user status changes to admin

--- pcps-client/app/src/main/java/de/pcps/jamtugether/utils/UiUtils.java
+++ pcps-client/app/src/main/java/de/pcps/jamtugether/utils/UiUtils.java
@@ -2,14 +2,22 @@ package de.pcps.jamtugether.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
 
 import androidx.annotation.DimenRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import de.pcps.jamtugether.R;
 
@@ -45,6 +53,21 @@ public class UiUtils {
 
     public static void showConfirmationDialog(@NonNull Context context, @StringRes int title, @StringRes int message, @NonNull OnDialogPositiveButtonClickedCallback callback) {
         showConfirmationDialog(context, context.getString(title), context.getString(message), callback);
+    }
+
+    public static void showSnackbar(@NonNull View view, @StringRes int message, int duration) {
+        Snackbar snackbar = Snackbar.make(view, message, duration);
+        snackbar.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE);
+        snackbar.setBackgroundTint(ContextCompat.getColor(view.getContext(), R.color.snackbarBackgroundColor));
+        snackbar.setTextColor(ContextCompat.getColor(view.getContext(), R.color.snackbarTextColor));
+        snackbar.setText(message);
+
+        View snackbarView = snackbar.getView();
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) snackbarView.getLayoutParams();
+        params.gravity = Gravity.TOP;
+        snackbarView.setLayoutParams(params);
+
+        snackbar.show();
     }
 
     public interface OnDialogPositiveButtonClickedCallback {
