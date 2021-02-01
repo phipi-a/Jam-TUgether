4cae696eff95673b25eacfde4288e2385103a43f
Tomer <t.yavor@gmx.net>
2021-02-01 16:57:33 +0100

    updated configure-file

--- pcps-server/configure-script.js
+++ pcps-server/configure-script.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 const fs = require('fs')
+const shell = require('shelljs')
 
 const atsecret = crypto.randomBytes(16).toString('hex')
 
@@ -8,4 +9,16 @@ fs.writeFile('.env', 'ACCESS_TOKEN_SECRET=' + atsecret, function (err, data) {
     return console.log('Creating enviroment file failed!')
   }
   return console.log(' Configuration was successfull')
+})
+
+shell.exec('openssl req -nodes -new -x509 -keyout server.key -out server.cert', (error, stdout, stderr) => {
+  if (error) {
+    console.log('failed to create server key')
+    return
+  }
+  if (stderr) {
+    console.log(' failed to create server key')
+    return
+  }
+  console.log('Success!')
 })
