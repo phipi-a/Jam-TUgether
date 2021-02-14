c1b1e3c850215f109e73241431c9c646ca7d317a
Philipp Pirlet <pirlet@campus.tu-berlin.de>
2021-02-14 21:15:32 +0100

    fix(flute audio): update audio files seamless loop

--- pcps-client/app/src/main/java/de/pcps/jamtugether/audio/instrument/flute/FluteSoundPool.java
+++ pcps-client/app/src/main/java/de/pcps/jamtugether/audio/instrument/flute/FluteSoundPool.java
@@ -9,7 +9,6 @@ import de.pcps.jamtugether.audio.sound.BaseSoundPool;
 public class FluteSoundPool extends BaseSoundPool {
 
     public FluteSoundPool(@NonNull Context context) {
-        super(context, 1, FluteSound.values(), 0);
-        // todo change loop to -1
+        super(context, 1, FluteSound.values(), -1);
     }
 }
